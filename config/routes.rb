4998e9a5e5c83edec1923336a028ccb3f1fdd1d4
Jay-Aye <jay@Jays-MacBook-Pro.local>
2017-05-21 17:33:19 +1000

    Initial Commit
    Create audiobook from pdf

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  #devise_for :users
+  resources :pages
+  resources :audiobooks
+  resources :users
+
+  get 'audiobooks/show'
+
+  get 'audiobooks/new'
+
+  get 'users/libraries'
+
+  get 'home/index'
+
+
+  root 'audiobooks#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
